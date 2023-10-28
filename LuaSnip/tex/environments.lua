992711df53d1daf2d94182644870c53d5fd49988
Nilay Tripathi <ntripathi2003@gmail.com>
2023-10-27 23:43:49 -0400

    Add snippets for beamer blocks

--- LuaSnip/tex/environments.lua
+++ LuaSnip/tex/environments.lua
@@ -210,4 +210,82 @@ return {
 				)
 		),
 
+		-- Beamer block 
+		s(
+				{trig = "!bl", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{block}{<>}
+										<>
+								\end{block}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
+		-- Alert block 
+		s(
+				{trig = "!albl", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{alertblock}{<>}
+										<>
+								\end{alertblock}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
+		-- Examples block
+		s(
+				{trig = "!blex", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{example}{<>}
+										<>
+								\end{example}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
+		-- Theorem block 
+		s(
+				{trig = "!blthm", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{theorem}{<>}
+										<>
+								\end{theorem}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
+		-- Definition block 
+		s(
+				{trig = "!bldefn", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{definition}{<>}
+										<>
+								\end{definition}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
+		-- Proof block 
+		s(
+				{trig = "!blpf", snippetType = "autosnippet"},
+				fmta(
+						[[
+								\begin{proof}{<>}
+										<>
+								\end{proof}
+						]],
+						{i(1), i(2)}
+				)
+		),
+
 }
